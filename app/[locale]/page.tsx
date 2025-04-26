ed627bd5d8a92eddb36e3de9ad892ca0c3111ca1
keevlabby <kevinollomy@gmail.com>
2025-04-26 03:32:24 +0300

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { ChatbotUISVG } from "@/components/icons/chatbotui-svg"
+import { ChatwitkevSVG } from "@/components/icons/chatwitkev-svg"
 import { IconArrowRight } from "@tabler/icons-react"
 import { useTheme } from "next-themes"
 import Link from "next/link"
@@ -11,16 +11,16 @@ export default function HomePage() {
   return (
     <div className="flex size-full flex-col items-center justify-center">
       <div>
-        <ChatbotUISVG theme={theme === "dark" ? "dark" : "light"} scale={0.3} />
+        <ChatwitkevSVG theme={theme === "dark" ? "dark" : "light"} scale={0.3} />
       </div>
 
-      <div className="mt-2 text-4xl font-bold">Chatbot UI</div>
+      <div className="mt-2 text-4xl font-bold">Chatwitkev</div>
 
       <Link
         className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-blue-500 p-2 font-semibold"
         href="/login"
       >
-        Start Chatting
+        Start Chatting, chapchap!
         <IconArrowRight className="ml-1" size={20} />
       </Link>
     </div>
