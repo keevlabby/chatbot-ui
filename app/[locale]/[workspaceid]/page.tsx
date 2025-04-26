251154f93a594c115023f41c1ba62d33686d1536
keevlabby <kevinollomy@gmail.com>
2025-04-26 03:40:00 +0300

    Update page.tsx

--- app/[locale]/[workspaceid]/page.tsx
+++ app/[locale]/[workspaceid]/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import { ChatbotUIContext } from "@/context/context"
+import { ChatwitkevContext } from "@/context/context"
 import { useContext } from "react"
 
 export default function WorkspacePage() {
-  const { selectedWorkspace } = useContext(ChatbotUIContext)
+  const { selectedWorkspace } = useContext(ChatwitkevContext)
 
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
